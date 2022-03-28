4e1c49ecb5e09e07fe0a3c43e829ff1ce448c3db
caiji <caiji202111@gmail.com>
2022-03-28 14:58:52 +0800

    frist commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+  <Card></Card>
+ <div class="home">
+  <VForm v-slot="{ errors, values, validate }" @submit="onSubmit">
+   {{ errors }} {{ values }}
+   <div class="mb-3">
+    <label for="email" class="form-label">Email</label>
+    <VField id="email" name="email" type="email"
+     class="form-control" :class="{ 'is-invalid': errors['email'] }"
+     placeholder="請輸入 Email" rules="email|required" v-model="user.email"></VField>
+    <error-message name="email" class="invalid-feedback"></error-message>
+   </div>
+   <button class="btn me-2 btn-outline-primary" type="button" @click="validate">驗證</button>
+   <button class="btn btn-primary" type="submit">Submit</button>
+  </VForm>
+ </div>
+</template>
+
+<script>
+import Card from '@/components/ComponentCard.vue'
+
+export default {
+  data () {
+    return {
+      user: {}
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log(this.user)
+    }
+  },
+  components: {
+    Card
+  }
+}
+
+</script>
